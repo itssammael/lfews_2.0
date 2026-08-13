8e05e096b6f3823ce08cd22b36cca8b5c6820c04
Alamein Tabunda <prohackerz609@gmail.com>
2026-08-13 09:16:41 +0800

    chore: disable registration and password reset features and update logo height styling

--- resources/js/Components/AuthenticationCardLogo.vue
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -5,7 +5,7 @@ import { Link } from '@inertiajs/vue3';
 <template>
     <Link :href="'/'">
         <template v-if="$page.props.system_settings?.system_logo">
-            <img :src="$page.props.system_settings.system_logo" alt="Logo" class="size-16" />
+            <img :src="$page.props.system_settings.system_logo" alt="Logo" class="h-16" />
         </template>
         <template v-else>
             <img src="/images/logo.png" alt="Logo" class="size-16" />
